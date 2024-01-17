0e6a021deeab749da0ceedb6a61658749f71cf4b
Rônero <52432170+ronerog@users.noreply.github.com>
2024-01-17 16:09:10 -0300

    add: agendamento whats

---
+++ src/components/calendar.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './calendar.css'
+
+const AgendamentoForm = () => {
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    const { nome, email } = formData;
+
+    if (![nome, email ].every((value) => value.trim())) {
+      alert('Todos os campos devem ser preenchidos');
+    } else {
+      const message = `
+        Olá, tudo bem? Vim pelo site e gostaria de marcar uma consulta contigo.
+        Nome: ${nome}
+        E-mail: ${email}
+      `;
+      const url = `https://wa.me/5582999520233?text=${encodeURIComponent(message)}`;
+      window.location.href = url;
+    }
+  };
+
+  return (
+    <div className="container">
+    <form onSubmit={(e) => e.preventDefault()} method="post">
+      <div className="card">
+      <a class="login">Agendamento</a>
+      
+
+    
+    <div className="inputBox">
+        <input required="required" type="text" value={formData.nome} onChange={handleChange} />
+        <span className='user'>Digite seu nome</span>
+      </div>
+      <div className="inputBox">
+        <input type="email" name="email" required="required" value={formData.email} onChange={handleChange} />
+        <span className='user'>Digite seu E-mail</span>
+        </div>
+        <button type="button" className='enter' onClick={handleSubmit}>
+        Agendar
+      </button>
+      </div>
+      <div>
+      </div>
+
+      
+    </form>
+    </div>
+  );
+};
+
+export default AgendamentoForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react"
 import "../../global.css"
 import logotipo from '../images/logotipo-principal-rosa.png'
-import minilogo from "../images/minilogo-cor.png"
+import minilogo from "../images/maca-dagua-branca.png"
+import AgendamentoForm from "../components/calendar"
 
 const pageStyles = {
   color: "#232129",
@@ -10,9 +11,21 @@ const pageStyles = {
 }
 
 const headingStyles = {
-  marginTop: 0,
+  marginTop: 30,
   marginBottom: 64,
   maxWidth: 420,
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '50%'
+}
+
+const calendarStyles = {
+ 
+  marginTop: 0,
+  marginBottom: 64,
   marginLeft: 'auto',
   marginRight: 'auto',
   display: 'flex',
@@ -21,11 +34,8 @@ const headingStyles = {
 }
 
 const minilog = {
-  marginTop: 60,
-  marginBottom: 64,
-  maxWidth: 80,
+  width: 150,
   marginLeft: 1,
-  marginRight: 'auto',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
@@ -38,118 +48,6 @@ const text = {
   justifyContent: 'center',
   alignItems: 'center',
 }
-// const headingAccentStyles = {
-//   color: "#663399",
-// }
-// const paragraphStyles = {
-//   marginBottom: 48,
-// }
-// const codeStyles = {
-//   color: "#8A6534",
-//   padding: 4,
-//   backgroundColor: "#FFF4DB",
-//   fontSize: "1.25rem",
-//   borderRadius: 4,
-// }
-// const listStyles = {
-//   marginBottom: 96,
-//   paddingLeft: 0,
-// }
-// const listItemStyles = {
-//   fontWeight: 300,
-//   fontSize: 24,
-//   maxWidth: 560,
-//   marginBottom: 30,
-// }
-
-// const linkStyle = {
-//   color: "#8954A8",
-//   fontWeight: "bold",
-//   fontSize: 16,
-//   verticalAlign: "5%",
-// }
-
-// const docLinkStyle = {
-//   ...linkStyle,
-//   listStyleType: "none",
-//   marginBottom: 24,
-// }
-
-// const descriptionStyle = {
-//   color: "#232129",
-//   fontSize: 14,
-//   marginTop: 10,
-//   marginBottom: 0,
-//   lineHeight: 1.25,
-// }
-
-// const docLink = {
-//   text: "Documentation",
-//   url: "https://www.gatsbyjs.com/docs/",
-//   color: "#8954A8",
-// }
-
-// const badgeStyle = {
-//   color: "#fff",
-//   backgroundColor: "#DFC2BE",
-//   border: "1px solid #088413",
-//   fontSize: 11,
-//   fontWeight: "bold",
-//   letterSpacing: 1,
-//   borderRadius: 4,
-//   padding: "4px 6px",
-//   display: "inline-block",
-//   position: "relative",
-//   top: -2,
-//   marginLeft: 10,
-//   lineHeight: 1,
-// }
-
-// const links = [
-//   {
-//     text: "Tutorial",
-//     url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-//     description:
-//       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-//     color: "#E95800",
-//   },
-//   {
-//     text: "How to Guides",
-//     url: "https://www.gatsbyjs.com/docs/how-to/",
-//     description:
-//       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-//     color: "#1099A8",
-//   },
-//   {
-//     text: "Reference Guides",
-//     url: "https://www.gatsbyjs.com/docs/reference/",
-//     description:
-//       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-//     color: "#BC027F",
-//   },
-//   {
-//     text: "Conceptual Guides",
-//     url: "https://www.gatsbyjs.com/docs/conceptual/",
-//     description:
-//       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-//     color: "#0D96F2",
-//   },
-//   {
-//     text: "Plugin Library",
-//     url: "https://www.gatsbyjs.com/plugins",
-//     description:
-//       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-//     color: "#8EB814",
-//   },
-//   {
-//     text: "Build and Host",
-//     url: "https://www.gatsbyjs.com/cloud",
-//     badge: true,
-//     description:
-//       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-//     color: "#663399",
-//   },
-// ]
 
 const IndexPage = () => {
   return (
@@ -168,6 +66,9 @@ const IndexPage = () => {
 </nav>
     <main style={pageStyles}>
       <img style={headingStyles} src={logotipo} alt="Logomarca" />
+      <div style={calendarStyles}>
+        <AgendamentoForm/>
+      </div>
     </main>
     </>
   )
