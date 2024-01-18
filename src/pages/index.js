9c0d0b41defb3c66227d944332a86b7f588460d3
Rônero <52432170+ronerog@users.noreply.github.com>
2024-01-17 22:08:55 -0300

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,6 @@ import logotipo from '../images/logotipo-principal-rosa.png'
 import minilogo from "../images/maca-dagua-branca.png"
 import AgendamentoForm from "../components/calendar"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-
-const headingStyles = {
-  marginTop: 30,
-  marginBottom: 64,
-  maxWidth: 420,
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderRadius: '50%'
-}
-
 const calendarStyles = {
  
   marginTop: 0,
@@ -34,16 +16,8 @@ const calendarStyles = {
 }
 
 const minilog = {
-  width: 150,
-  marginLeft: 1,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-}
-
-const text = {
-  marginTop: 60,
-  color: "#FFFF",
+  width: 70,
+  marginLeft: 20,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
@@ -64,8 +38,11 @@ const IndexPage = () => {
       </ul>
   </div>
 </nav>
-    <main style={pageStyles}>
-      <img style={headingStyles} src={logotipo} alt="Logomarca" />
+    <main>
+      <div className="banner">
+      {/* <img className="headingStyles" src={logotipo} alt="Logomarca" /> */}
+      </div>
+      
       <div style={calendarStyles}>
         <AgendamentoForm/>
       </div>
@@ -76,4 +53,4 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Mariana Lima - Fisioterapia</title>
